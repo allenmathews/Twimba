46f106adfc5d9d7e149e70fdbc5187c1671a0c13
Allen Mathews <allenjoemathews@gmail.com>
2023-06-29 17:58:18 +1000

    added data.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function() {
+            console.log(tweetInput.value)
