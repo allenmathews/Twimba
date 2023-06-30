4fb157cfaad202784a824e93a6afeb891a92faaf
Allen Mathews <allenjoemathews@gmail.com>
2023-06-30 18:19:31 +1000

    added textarea

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { tweetsData } from './data.js'
-const tweetInput = document.getElementById('tweet-input')
-const tweetBtn = document.getElementById('tweet-btn')
+import { tweetsData } from './data'
+// const tweetInput = document.getElementById('tweet-input')
+// const tweetBtn = document.getElementById('tweet-btn')
 
-tweetBtn.addEventListener('click', function() {
-            console.log(tweetInput.value)
+// tweetBtn.addEventListener('click', function() {
+//     console.log(tweetInput.value)
+// })
+
+console.log(tweetsData)
